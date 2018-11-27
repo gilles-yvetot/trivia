3aff98ca596ea7cd684cd529033e489239e3057a
Gilles Yvetot <g.yvetot@gmail.com>
2018-11-27 13:24:36 +0700

    [ADD] basic style

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { connect } from 'unistore/react'
-
+import Wheel from '../components/Wheel'
+import Question from '../components/Question'
 import actions from '../store/actions'
 
-
 const styles = () => ({
+  root: {
+    display: 'flex',
+    flex: '1',
+    padding: '20px',
+    fontFamily: 'Roboto, sans-serif',
+    background: '#eaeaea',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
 });
 
 class Index extends React.Component {
   state = {
   };
 
+  onCategorySelected = category => {
+    this.props.setCategory(category)
+  }
+
   render() {
     const {
       classes,
       user,
+      question,
     } = this.props;
-    return (
-      <div className={classes.root}>
-        Hello&nbsp;
-        {user && user.email || ''}
-      </div>
-    );
+    if (user) {
+      return (
+        <div className={classes.root}>
+          <Wheel onCategorySelected={this.onCategorySelected} />
+          {question && <Question question={question} />}
+        </div>
+      );
+    }
+    return null
   }
 }
 Index = connect(store => store, actions)(Index)
