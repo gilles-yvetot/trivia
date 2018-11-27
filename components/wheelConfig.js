a4373f9c2d6ea8e5a3aa25be1d6cff31d4cc8b59
Gilles Yvetot <g.yvetot@gmail.com>
2018-11-27 14:32:11 +0700

    [FIX] transparent should not be included

--- components/wheelConfig.js
+++ components/wheelConfig.js
@@ -1,32 +1,32 @@
 export default [
   {
     'icon': 'fa fa-atom',
-    'color': '#16a085 transparent',
+    'color': '#16a085',
     data: 17
   },
   {
     'icon': 'fa fa-lightbulb',
-    'color': '#2980b9 transparent',
+    'color': '#2980b9',
     data: 9
   },
   {
     'icon': 'fa fa-trophy',
-    'color': '#34495e transparent',
+    'color': '#34495e',
     data: 21
   },
   {
     'icon': 'fa fa-globe-americas',
-    'color': '#f39c12 transparent',
+    'color': '#f39c12',
     data: 22,
   },
   {
     'icon': 'fa fa-book',
-    'color': '#d35400 transparent',
+    'color': '#d35400',
     data: 23,
   },
   {
     'icon': 'fa fa-film',
-    'color': '#c0392b transparent',
+    'color': '#c0392b',
     data: 11,
   }
 ]
