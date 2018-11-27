e568ee6e3f773724589dcf407b4bfe8c0ff21eb5
Gilles Yvetot <g.yvetot@gmail.com>
2018-11-27 13:24:21 +0700

    [UPG] font awesome

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
         <Head>
           <link rel='icon' href='https://omniinc.com/favicon.ico' />
           <link href="https://fonts.googleapis.com/css?family=Oxygen:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" />
-          <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin='anonymous' />
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossOrigin="anonymous" />
           <meta charSet="utf-8" />
           <meta
             name="viewport"
